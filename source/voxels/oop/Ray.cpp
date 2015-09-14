3ab234964324bdb8ef44a4dbf14ee5e861fa2d8d
Quinten <quintenlansu@gmail.com>
2015-09-14 09:18:32 +0200

    Refactor ray-voxel intersection.

--- source/voxels/oop/Ray.cpp
+++ source/voxels/oop/Ray.cpp
@@ -27,84 +27,55 @@ namespace Tmpl {
 
 	bool Ray::intersects(Voxel& other)
 	{
-		float t0, t1;
-		float tmin = 0.f;
-		float tmax = std::numeric_limits<float>::max();
+		glm::vec3 offsetMinimum = (other.getAABBMinimum() - m_origin) * m_direction;
+		glm::vec3 offsetMaximum = (other.getAABBMaximum() - m_origin) * m_direction;
 
-		t0 = (other.getAABBMinimum().x - m_origin.x) * m_direction.x;
-		t1 = (other.getAABBMaximum().x - m_origin.x) * m_direction.x;
-		if (t0 > t1)
+		float localTimeMinimum = 0.f;
+		float localTimeMaximum = std::numeric_limits<float>::max();
+
+		if (offsetMinimum.x > offsetMaximum.x)
 		{
-			tmin = (t1 > tmin) ? t1 : tmin;
-			tmax = (t0 < tmax) ? t0 : tmax;
+			localTimeMinimum = glm::max(offsetMaximum.x, localTimeMinimum);
+			localTimeMaximum = glm::min(offsetMinimum.x, localTimeMaximum);
 		}
 		else
 		{
-			tmin = (t0 > tmin) ? t0 : tmin;
-			tmax = (t1 < tmax) ? t1 : tmax;
+			localTimeMinimum = glm::max(offsetMinimum.x, localTimeMinimum);
+			localTimeMaximum = glm::min(offsetMaximum.x, localTimeMaximum);
 		}
 
-		t0 = (other.getAABBMinimum().y - m_origin.y) * m_direction.y;
-		t1 = (other.getAABBMaximum().y - m_origin.y) * m_direction.y;
-		if (t0 > t1)
+		if (offsetMinimum.y > offsetMaximum.y)
 		{
-			tmin = (t1 > tmin) ? t1 : tmin;
-			tmax = (t0 < tmax) ? t0 : tmax;
+			localTimeMinimum = glm::max(offsetMaximum.y, localTimeMinimum);
+			localTimeMaximum = glm::min(offsetMinimum.y, localTimeMaximum);
 		}
 		else
 		{
-			tmin = (t0 > tmin) ? t0 : tmin;
-			tmax = (t1 < tmax) ? t1 : tmax;
+			localTimeMinimum = glm::max(offsetMinimum.y, localTimeMinimum);
+			localTimeMaximum = glm::min(offsetMaximum.y, localTimeMaximum);
 		}
 
-		t0 = (other.getAABBMinimum().z - m_origin.z) * m_direction.z;
-		t1 = (other.getAABBMaximum().z - m_origin.z) * m_direction.z;
-		if (t0 > t1)
+		if (offsetMinimum.z > offsetMaximum.z)
 		{
-			tmin = (t1 > tmin) ? t1 : tmin;
-			tmax = (t0 < tmax) ? t0 : tmax;
+			localTimeMinimum = glm::max(offsetMaximum.z, localTimeMinimum);
+			localTimeMaximum = glm::min(offsetMinimum.z, localTimeMaximum);
 		}
 		else
 		{
-			tmin = (t0 > tmin) ? t0 : tmin;
-			tmax = (t1 < tmax) ? t1 : tmax;
+			localTimeMinimum = glm::max(offsetMinimum.z, localTimeMinimum);
+			localTimeMaximum = glm::min(offsetMaximum.z, localTimeMaximum);
 		}
 
-		if (tmin <= tmax && tmin <= m_timeMinimum)
+		if (localTimeMinimum <= localTimeMaximum &&
+			localTimeMinimum <= m_timeMinimum)
 		{
-			m_timeMinimum = tmin;
+			m_timeMinimum = localTimeMinimum;
 			m_closest = &other;
 
 			return true;
 		}
 
 		return false;
-
-		/*glm::vec3 localMinimum = (other.getAABBMinimum() - m_origin) * m_direction;
-		glm::vec3 localMaximum = (other.getAABBMaximum() - m_origin) * m_direction;
-
-		glm::vec3 distanceMinimum = glm::min(localMinimum, localMaximum);
-		glm::vec3 distanceMaximum = glm::max(localMinimum, localMaximum);
-
-		float localTimeMinimum = 0.0f;
-		localTimeMinimum = glm::max(localTimeMinimum, distanceMinimum.x);
-		localTimeMinimum = glm::max(localTimeMinimum, distanceMinimum.y);
-		localTimeMinimum = glm::max(localTimeMinimum, distanceMinimum.z);
-
-		float localTimeMaximum = std::numeric_limits<float>::max();
-		localTimeMaximum = glm::min(localTimeMaximum, distanceMaximum.x);
-		localTimeMaximum = glm::min(localTimeMaximum, distanceMaximum.y);
-		localTimeMaximum = glm::min(localTimeMaximum, distanceMaximum.z);
-
-		if (localTimeMinimum <= localTimeMaximum &&
-			localTimeMinimum <= m_timeMinimum)
-		{
-			m_timeMinimum = localTimeMinimum;
-
-			return true;
-		}
-
-		return false;*/
 	}
 
 }; // namespace Tmpl
