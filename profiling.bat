f4c9b74cbeb7eb853ce897fbe46117aaccc2bc48
Quinten <quintenlansu@gmail.com>
2015-09-17 15:07:10 +0200

    Find available executable for running the batch script.

--- profiling.bat
+++ profiling.bat
@@ -1,6 +1,17 @@
-@"output/windows/Win32/Release/DODExample.exe" -profiling 100 -ood -scene small
-@"output/windows/Win32/Release/DODExample.exe" -profiling 100 -dod -scene small
-@"output/windows/Win32/Release/DODExample.exe" -profiling 100 -ood -scene medium
-@"output/windows/Win32/Release/DODExample.exe" -profiling 100 -dod -scene medium
-@"output/windows/Win32/Release/DODExample.exe" -profiling 100 -ood -scene large
-@"output/windows/Win32/Release/DODExample.exe" -profiling 100 -dod -scene large
+@IF EXIST "output/windows/x64/Release/DODExample.exe" (
+	@ECHO Using 64-bit executable.
+	@SET PROFILE_EXE="output/windows/x64/Release/DODExample.exe"
+) ELSE IF EXIST "output/windows/Win32/Release/DODExample.exe" (
+	@ECHO Using 32-bit executable.
+	@SET PROFILE_EXE="output/windows/Win32/Release/DODExample.exe"
+) ELSE (
+	@ECHO Missing executable, compile it from source!
+	@EXIT /B 0
+)
+
+@%PROFILE_EXE% -profiling 100 -ood -scene small
+@%PROFILE_EXE% -profiling 100 -dod -scene small
+@%PROFILE_EXE% -profiling 100 -ood -scene medium
+@%PROFILE_EXE% -profiling 100 -dod -scene medium
+@%PROFILE_EXE% -profiling 100 -ood -scene large
+@%PROFILE_EXE% -profiling 100 -dod -scene large
